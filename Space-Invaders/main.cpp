2028d6ebf4e455a345059505f49dbe7793187ce8
Tarun <tarunkumarsharma38@gmail.com>
2024-07-13 00:03:44 +0530

    MODULE 2 INTRO TO OOPS
    ASSIGNMENT STARTING OFF WITH THE PLAYER

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -1,4 +1,23 @@
+#include<SFML/Graphics.hpp>
+#include<iostream>
+using namespace std;
+class Player {
 
+private:
+
+    int health;
+    int playerscore;
+    sf::Vector2f position;
+public:
+    sf::Texture texture;
+    sf::Sprite sprite;
+
+
+    void TakeDamage();
+    void move();
+    void shootbullets();
+
+};
 int main()
 {
     return 0;
