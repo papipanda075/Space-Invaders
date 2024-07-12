db682cd711f15cd6c07b60a60f924168ac6e5dc6
Tarun <tarunkumarsharma38@gmail.com>
2024-07-13 00:17:07 +0530

    Update main.cpp
    ASSIGNEMENT  FETCH KEYBOARD I/P

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -19,6 +19,35 @@ public:
 
 };
 int main()
+
 {
+    sf::VideoMode videomode = sf::VideoMode(1920, 1080);
+    sf::RenderWindow window(videomode, "SPACE INVADER");
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+
+        window.clear(sf::Color::Black);
+
+        Player One;
+        One.texture.loadFromFile("assets/textures/player_ship.png");
+        One.sprite.setTexture(One.texture);
+        One.sprite.setPosition(960, 540);
+
+        sf::Keyboard::isKeyPressed(sf::Keyboard::Right);
+
+        sf::Keyboard::isKeyPressed(sf::Keyboard::Left);
+
+
+   
+        window.draw(One.sprite);
+        window.display();
+
+    }
+
     return 0;
 }
