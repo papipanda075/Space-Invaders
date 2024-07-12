1be94ad043f69fb98ffefb38ee84ecfc3d6885f2
Tarun <tarunkumarsharma38@gmail.com>
2024-07-12 23:09:22 +0530

    ASSIGNEMNT GAME WINDOW

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -3,6 +3,23 @@ int main()
 {
     sf::VideoMode video = sf::VideoMode(800, 600);
     sf::RenderWindow window(video, "SPACE INVADERS");
+   
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            // Check for window closure
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
 
+
+        // Clear the window
+        window.clear(sf::Color::Blue);
+
+        // Display whatever you draw
+        window.display();
+    }
+
+ 
     return 0;
 }
