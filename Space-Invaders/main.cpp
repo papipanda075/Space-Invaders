de80dc6abf2564262d60b4246fe24b037e93485c
Tarun <tarunkumarsharma38@gmail.com>
2024-07-12 23:35:50 +0530

    add shapes of the different kinds

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -1,4 +1,7 @@
 #include<SFML/Graphics.hpp>
+#include<iostream>
+#include<cmath>
+using namespace std;
 int main()
 {
     sf::VideoMode video = sf::VideoMode(800, 600);
@@ -14,9 +17,29 @@ int main()
 
 
         // Clear the window
-        window.clear(sf::Color::Blue);
+        window.clear(sf::Color::Black);
 
+        sf::CircleShape circle(50);
+        circle.setFillColor(sf::Color::Green);
+        circle.setPosition(400, 300);
+
+        sf::RectangleShape square(sf::Vector2f(100,100));
+        square.setFillColor(sf::Color::Red);
+        square.setPosition(1, 0);
+        
+
+        sf::ConvexShape triangle;
+        triangle.setPointCount(3);
+        int size =100;
+        triangle.setPoint(0, sf::Vector2f(0, 0)); // Top vertex
+        triangle.setPoint(1, sf::Vector2f(size, 0)); // Bottom-right vertex
+        triangle.setPoint(2, sf::Vector2f(size / 2.f, std::sqrt(3.f) / 2.f * size));
+        triangle.setFillColor(sf::Color::Blue);
+        triangle.setPosition(750-size/2,0);
         // Display whatever you draw
+        window.draw(circle);
+        window.draw(square);
+        window.draw(triangle);
         window.display();
     }
 
