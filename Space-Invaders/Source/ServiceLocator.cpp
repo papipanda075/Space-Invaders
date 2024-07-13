77a3770ec71216925a1acc95895275e1bfcab807
Tarun <tarunkumarsharma38@gmail.com>
2024-07-13 17:03:32 +0530

    Created Service Locator to make the communication between units easy

---
+++ Space-Invaders/Source/ServiceLocator.cpp
@@ -0,0 +1,30 @@
+#include"../Header/ServiceLocator.h"
+using namespace std;
+
+void ServiceLocator::createServices() {
+
+}
+void ServiceLocator::clearAllServices() {
+
+}
+
+ServiceLocator::ServiceLocator() {
+
+}
+
+ServiceLocator::~ServiceLocator() {
+
+}
+
+void ServiceLocator::initialize(){
+
+}
+
+void ServiceLocator::update() {
+
+}
+
+void::ServiceLocator::render() {
+
+}
+static ServiceLocator* getInstance();
