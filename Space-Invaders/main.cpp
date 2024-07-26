46d3eb914ebec96c25f0997df65c4dece32700c2
Tarun <tarunkumarsharma38@gmail.com>
2024-07-26 19:48:55 +0530

    The whole file gets corrupted thats why i  have to write all programs till player ship again

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -1,5 +1,20 @@
-
+#include "Headers/GameService.h"
 int main()
 {
+
+    GameService* game_service = new GameService();
+
+    game_service->ignite();
+
+    while (game_service->isRunning())
+    {
+        game_service->update();
+        game_service->render();
+
+
+
+
+    }
     return 0;
 }
+
